bf74aeca1e5498c0cb14cce1863f7ca3d66c7d8d
Wesley <angelportinari17@gmail.com>
2019-11-13 19:02:52 -0300

    Added Momentjs in Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,12 @@
 const router = require("express").Router();
-const {
-  query,
-  param,
-  validationResult,
-  matchedData
-} = require("express-validator");
 
-const CityRepository = require("../repositories/city_repository");
 const moment = require("moment");
 
 // Get All Cities
-router.get("/cities/get", (req, res) => {
-  res.json(CityRepository.getAll());
-});
+router.get("/cities/get", (req, res) => {});
 
 // Get Cities if it has Weathers
-router.get("/weather/cities", (req, res) => {
-  res.json(CityRepository.getCitiesIfHasWeather());
-});
+router.get("/weather/cities", (req, res) => {});
 
 // Get City with Weathers
 router.get(
@@ -31,49 +20,7 @@ router.get(
       .optional({ checkFalsy: true })
       .escape()
   ],
-  (req, res) => {
-    let result = [];
-
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
-    }
-
-    const cityId = Number(req.params.id);
-    const { start_date, end_date } = matchedData(req, {
-      includeOptionals: false
-    });
-
-    // StartDate
-    const startDate =
-      start_date && moment(start_date, "YYYY-MM-DD").isValid()
-        ? moment(req.query.start_date, "YYYY-MM-DD").unix()
-        : false;
-
-    // EndDate
-    const endDate =
-      end_date && moment(end_date, "YYYY-MM-DD").isValid()
-        ? moment(req.query.end_date, "YYYY-MM-DD").unix()
-        : false;
-
-    if (startDate || endDate) {
-      if (startDate && endDate && endDate < startDate) {
-        return res
-          .status(422)
-          .json({ msg: "end_date must be ever bigger or equal start_date" });
-      }
-
-      result = CityRepository.getCityWithWeatherBetweenDates(
-        cityId,
-        start_date ? startDate : null,
-        end_date ? endDate : null
-      );
-    } else {
-      result = CityRepository.getCityWithWeather(cityId);
-    }
-
-    return res.json(result);
-  }
+  (req, res) => {}
 );
 
 module.exports = router;

c3f1e7cc507bd9168d99175c19db2b27766e8fa6
Wesley <angelportinari17@gmail.com>
2019-11-13 19:24:14 -0300

    created getCityWithWeather and getCityWithWeatherBetweenDates in CityRepository and getCityWeatherBetweenDates in WeatherRepository

--- src/repositories/city_repository.js
+++ src/repositories/city_repository.js
@@ -4,6 +4,29 @@ const { City: Cities } = require("../database/load_json_database");
 const CityRepository = {
   getAll: () => {
     return Cities;
+  },
+
+  getCitiesIfHasWeather: () => {
+    const cityIds = WeatherRepository.getAvailableCityIds();
+    const cities = _.filter(Cities, city => {
+      if (cityIds.includes(city.id)) {
+        return city;
+      }
+    });
+
+    return cities.map(city => {
+      return {
+        data: cities,
+        weather: WeatherRepository.getCityWeather(city.id)
+      };
+    });
+  },
+
+  getCityWithWeather: cityId => {
+    return {
+      data: _.find(Cities, { id: cityId }) || [],
+      weather: WeatherRepository.getCityWeather(cityId)
+    };
   }
 };
 
--- src/repositories/weather_repository.js
+++ src/repositories/weather_repository.js
@@ -11,6 +11,40 @@ const WeatherRepository = {
 
   getAvailableCityIds: () => {
     return _.map(Weathers, "cityId");
+  },
+
+  getCityWeatherBetweenDates: (cityId, startDate, endDate) => {
+    if (!cityId) return false;
+
+    const foundWeather = _.find(Weathers, { cityId });
+    let weatherList = [];
+
+    if (startDate && endDate && endDate < startDate) return false;
+
+    if (foundWeather) {
+      weatherList = _.filter(foundWeather.data, weather => {
+        const weather_timestamp = moment(
+          moment.unix(weather.dt).format("DD/MM/YYYY"),
+          "DD/MM/YYYY"
+        ).unix();
+
+        if (!endDate && startDate) {
+          if (weather_timestamp >= startDate) {
+            return weather;
+          }
+        } else if (endDate && !startDate) {
+          if (weather_timestamp <= endDate) {
+            return weather;
+          }
+        } else if (
+          weather_timestamp >= startDate &&
+          weather_timestamp <= endDate
+        ) {
+          return weather;
+        }
+      });
+    }
+    return weatherList;
   }
 };
 
